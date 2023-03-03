da2f3deff3eb29cc10bbd7bbf5e57216aa3e5216
Benson\Benson <a092791097@gmail.com>
2023-03-03 20:41:35 +0800

    格式化測試-11

--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,7 @@
 console.log('test');
-function test() {console.log('ss');console.log('ss');console.log('ss');}
+function test() {
+  console.log('ss');
+  console.log('ss');
+  console.log('ss');
+  console.log('ss');
+}
